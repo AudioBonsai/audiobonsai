057c8a385e9f43dcbff6fc488b1beb3ddfe75b07
Jesse Erdmann <jesse@jesseerdmann.com>
2019-01-18 08:34:54 -0600

    Add the spotipy credential cache to git ignore, finished loading albums, including a table rename, and started extracting artists from the album json.

--- sorting_hat_cli.py
+++ sorting_hat_cli.py
@@ -1,4 +1,5 @@
 import codecs
+import json
 import os
 import re
 import sqlite3
@@ -14,10 +15,11 @@ from urllib.request import urlopen
 
 
 EVERYNOISE_URL = 'http://everynoise.com/spotify_new_releases.html'
-CREATE_CANDIDATE_TABLE = """ CREATE TABLE IF NOT EXISTS candidates (
+CREATE_CANDIDATE_TABLE = """ CREATE TABLE IF NOT EXISTS albums (
                                 spotify_uri text PRIMARY KEY,
                                 add_date DATE,
-                                json_text TEXT
+                                json_text TEXT,
+                                artists_extracted BOOLEAN default FALSE
                              ) """
 GROUP_TEXT = 'spotify:album:.* .* albumid=(spotify:album:.*) nolink=true ' \
              + 'onclick="playmeta.*'
@@ -69,7 +71,7 @@ def download_everynoise(today_dir):
         return html
 
 
-def find_candidates(html):
+def find_albums(html):
     today = datetime.now().strftime("%Y-%m-%d 00:00:00.000000")
     match_string = re.compile('(spotify:album:.*)')
     group_string = re.compile(GROUP_TEXT)
@@ -87,7 +89,7 @@ def find_candidates(html):
             spotify_uri = bits.group(1)
             candidate_list.add((spotify_uri, today, ''))
             # print("Spotify URI: {}".format(spotify_uri))
-    log("Found {:d} candidates".format(len(candidate_list)))
+    log("Found {:d} albums".format(len(candidate_list)))
     return candidate_list
 
 
@@ -99,14 +101,16 @@ def create_connection(today_dir):
         return conn
     except sqlError as e:
         print(e)
+        print(type(e))
 
 
-def insert_candidates(conn, candidate_list):
-    insert_stmt = 'INSERT INTO candidates VALUES(?,?,?)'
+def insert_albums(conn, candidate_list):
+    insert_stmt = 'INSERT INTO albums (spotify_uri, add_date, json_text)' \
+                  + ' VALUES(?,?,?)'
     try:
         log('Creating table')
         conn.execute(CREATE_CANDIDATE_TABLE)
-        log('Loading candidates')
+        log('Loading albums')
         for candidate in candidate_list:
             try:
                 conn.execute(insert_stmt, candidate)
@@ -114,37 +118,76 @@ def insert_candidates(conn, candidate_list):
                 # Ignore duplicates
                 pass
         conn.commit()
-        log('Candidates inserted')
+        log('Albums inserted')
     except Exception as e:
         print(e)
+        print(type(e))
         dir(e)
 
 
 def get_candidate_json(db_conn, sp_conn):
-    select_stmt = 'SELECT spotify_uri from candidates where json_text = ""'
-    log('Retrieving album JSON')
+    select_stmt = 'SELECT spotify_uri from albums where json_text = ""'
+    update_stmt = 'UPDATE albums set json_text = ? where spotify_uri = ?'
+    log('Retrieving albums to update')
     try:
         db_cursor = db_conn.cursor()
         db_cursor.execute(select_stmt)
         albums = db_cursor.fetchall()
     except Exception as e:
         print(e)
+        print(type(e))
 
     offset = 0
     batch_size = 20
+    update_list = set()
+    log('Requesting JSON for {:d} albums'.format(len(albums)))
     while offset < len(albums):
         sp_uri_list = [album[0] for album in
                        albums[offset:offset + batch_size]]
         try:
             album_dets_list = sp_conn.albums(sp_uri_list)
-            pprint(album_dets_list[u'albums'][0])
+            for album_dets in album_dets_list[u'albums']:
+                try:
+                    update_list.add((json.dumps(album_dets),
+                                     album_dets[u'uri']))
+                except TypeError as te:
+                    pass
         except Exception as e:
             print(e)
-        return
+            print(type(e))
         if offset > 0 and offset % 1000 == 0:
             log('-> {} albums retrieved'.format(offset))
         offset += batch_size
+        try:
+            db_conn.executemany(update_stmt, update_list)
+            db_conn.commit()
+            update_list = set()
+        except Exception as e:
+            print(e)
+            print(type(e))
+    log('Album JSON updated in database')
     log('Album JSON retrieved')
+
+
+def extract_artists(db_conn, sp_conn):
+    select_stmt = 'SELECT spotify_uri, json_text from albums' \
+                  + ' where artists_extracted = FALSE'
+    update_stmt = 'UPDATE albums set artists_extracted = TRUE' \
+                  + ' where spotify_uri = ?'
+    log('Retrieving albums to extract artists from')
+    try:
+        db_cursor = db_conn.cursor()
+        db_cursor.execute(select_stmt)
+        albums = db_cursor.fetchall()
+    except Exception as e:
+        print(e)
+        print(type(e))
+
+    for album in albums:
+        album_json = json.loads(album[1])
+        album_uri = album[0]
+        pprint(album_json)
+        break
 
 
 if __name__ == '__main__':
@@ -152,18 +195,20 @@ if __name__ == '__main__':
     # Get sorting hat and scrape album uris
     try:
         html = download_everynoise(today_dir)
-        candidates = find_candidates(html)
+        albums = find_albums(html)
     except Exception as e:
         print(e)
+        print(type(e))
         raise(e)
 
     db_conn = create_connection(today_dir)
     # Load albums into database
     try:
         cursor = db_conn.cursor()
-        insert_candidates(db_conn, candidates)
+        insert_albums(db_conn, albums)
     except Exception as e:
         print(e)
+        print(type(e))
         raise(e)
 
     try:
@@ -171,6 +216,15 @@ if __name__ == '__main__':
         get_candidate_json(db_conn, sp_conn)
     except Exception as e:
         print(e)
+        print(type(e))
+        raise(e)
+
+    try:
+        sp_conn = get_spotify_conn()
+        extract_artists(db_conn, sp_conn)
+    except Exception as e:
+        print(e)
+        print(type(e))
         raise(e)
 
     db_conn.close()
